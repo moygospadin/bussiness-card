8abece873f5e92ddd71a8a05ce6e3d5c1a987c0f
Your Name <you@example.com>
2023-10-08 15:11:16 +0200

    cringe anim

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Movement Animation Variables:
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
-
+const body = document.querySelector("body");
 const socialList = document.querySelector(".social-list");
 
 //Movement Animation Event:
@@ -26,3 +26,43 @@ container.addEventListener("mouseleave", (event) => {
 
   socialList.style.transform = "translateZ(0px)";
 });
+
+const UFO_TEMPLATE = `
+<div class="container-ufo">
+  <div class="spaceship">
+    <div class="robot">
+      <div id="half">
+        <div id="brain"></div>
+      </div>
+      <div id="head">
+        <span id="lc"></span><span id="le"></span><span id="re"></span>
+        <div id="mouth"></div>
+        <span id="rc"></span>
+      </div>
+      <div id="foot"></div>
+      <div id="leg-1"></div>
+      <div id="leg-2"></div>
+    </div>
+    <div class="spaceship_top"></div>
+    <div class="spaceship_middle">
+      <div class="spaceship_balls_1"></div>
+      <div class="spaceship_balls_2"></div>
+      <div class="spaceship_balls_3"></div>
+      <div class="spaceship_balls_4"></div>
+      <div class="spaceship_balls_5"></div>
+    </div>
+    <div class="spaceship_bottom">
+      <div class="abduct_hole1"></div>
+      <div class="abduct_hole2"></div>
+      <div class="abduct_hole3"></div>
+      <div class="abduct_hole4"></div>
+      <div class="abduct_hole5"></div>
+    </div>
+  </div>
+</div>
+`;
+
+body.insertAdjacentHTML("afterbegin", UFO_TEMPLATE);
+const containerUfo = document.querySelector(".container-ufo");
+setTimeout(() => containerUfo.classList.add('animation-fade-dissapear'), 5000);
+setTimeout(() => containerUfo.remove(), 7000);
